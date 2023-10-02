9702dce851f5469eee9a5c370a5f302c4a3effe6
Alex Power <alexpower9@gmail.com>
2023-10-02 17:15:52 -0400

    Quick fix for the inventory for optimization

--- src/Screens/PlayLevelScreen.java
+++ src/Screens/PlayLevelScreen.java
@@ -53,7 +53,7 @@ public class PlayLevelScreen extends Screen {
         this.player.setFacingDirection(Direction.LEFT);
 
         //setup inventory
-        this.inventory = new Inventory("noSelectionHUD.png", "oneSlotHUD.png", "twoSlotHUD.png", "threeSlotHUD.png", "fourSlotHUD.png");
+        this.inventory = new Inventory("noSelectionHUD.png", "oneSlotHUD.png", "twoSlotHUD.png", "threeSlotHUD.png", "fourSlotHUD.png",this.map);
 
         
         // let pieces of map know which button to listen for as the "interact" button
