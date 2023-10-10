103a3a9031c16ba73dff9698c2518268ed74f9d1
Alex Power <alexpower9@gmail.com>
2023-10-10 14:57:09 -0400

    Just some general updates, nothing major

---
+++ src/Screens/PauseScreen.java
@@ -0,0 +1,35 @@
+package Screens;
+
+import Engine.GraphicsHandler;
+import Engine.Screen;
+import Game.ScreenCoordinator;
+import Level.Map;
+import ui.Container.CenterContainer;
+
+public class PauseScreen extends Screen
+{
+    protected ScreenCoordinator screenCoordinator;
+    protected Map background;
+
+
+    protected CenterContainer centerContainer;
+
+    public PauseScreen(ScreenCoordinator screenCoordinator) {
+        this.screenCoordinator = screenCoordinator;
+    }
+
+    public void initialize()
+    {
+
+    }
+
+    public void draw(GraphicsHandler graphicsHandler)
+    {
+
+    }
+
+    public void update()
+    {
+
+    }
+}
