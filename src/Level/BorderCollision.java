1cd713a7ecc16a5946e4c5283d30732c7b6e8dd0
Tim McGinley <timmcginley0325@gmail.com>
2023-10-23 17:32:43 -0400

    - Dialogue and border changes

---
+++ src/Level/BorderCollision.java
@@ -0,0 +1,18 @@
+package Level;
+
+import Builders.FrameBuilder;
+import Engine.ImageLoader;
+import GameObject.GameObject;
+import GameObject.SpriteSheet;
+import GameObject.Frame;
+import Level.Player;
+import Maps.WildWestMap;
+import Utils.Point;
+
+
+public class BorderCollision {
+
+    // public static CollsionState side;
+
+}
+
