979f93c04e940d174e1c8e4eabc9d253c1cf1419
Chris <cjkilduff@quinnipiac.edu>
2023-10-25 19:24:45 -0400

    put menu button on top

--- src/Screens/ControlsScreen.java
+++ src/Screens/ControlsScreen.java
@@ -36,63 +36,62 @@ public class ControlsScreen extends Screen {
   public void initialize() {
     background = new TitleScreenMap();
     posContainer = new PositioningContainer(Anchor.TOP_CENTER);
-    UIContainer container = new UIContainer(0, 20, 0, 0);
     posContainer.setAnchorChildren(true);
     posContainer.setfillType(FillType.FILL_SCREEN);
-    container.addComponent(new SpriteUI(0, 0, ImageUtils.createFilledImage(800, 500, new Color(0, 0, 0, 100))));
+    posContainer.addComponent(new SpriteUI(0, 0, ImageUtils.createFilledImage(800, 500, new Color(0, 0, 0, 100))));
 
     // controls label
     controlsLabel = new SpriteFont("Controls", 0, 7, new Font("Times New Roman", Font.BOLD, 30), Color.ORANGE);
-    container.addComponent(controlsLabel);
+    posContainer.addComponent(controlsLabel);
 
     // movement label
     SpriteFont movementLabel = new SpriteFont("Movement", -250, 80, new Font("Times New Roman", Font.BOLD, 30),
         Color.ORANGE);
-    container.addComponent(movementLabel);
+    posContainer.addComponent(movementLabel);
 
     // movement controls
     SpriteFont movementControls = new SpriteFont("W-A-S-D", 250, 80, new Font("Times New Roman", Font.BOLD, 30),
         Color.ORANGE);
-    container.addComponent(movementControls);
+    posContainer.addComponent(movementControls);
 
     // interact label
     SpriteFont interactLabel = new SpriteFont("Interact/Pick-Up Items", -230, 150,
         new Font("Times New Roman", Font.BOLD, 30), Color.ORANGE);
-    container.addComponent(interactLabel);
+    posContainer.addComponent(interactLabel);
 
     // interact controls
     SpriteFont interactControls = new SpriteFont("Return", 250, 150, new Font("Times New Roman", Font.BOLD, 30),
         Color.ORANGE);
-    container.addComponent(interactControls);
+    posContainer.addComponent(interactControls);
 
     // pause label
     SpriteFont pauseLabel = new SpriteFont("Pause", -250, 220, new Font("Times New Roman", Font.BOLD, 30),
         Color.ORANGE);
-    container.addComponent(pauseLabel);
+    posContainer.addComponent(pauseLabel);
 
     // pause controls
     SpriteFont pauseControls = new SpriteFont("ESC", 250, 220, new Font("Times New Roman", Font.BOLD, 30),
         Color.ORANGE);
-    container.addComponent(pauseControls);
+    posContainer.addComponent(pauseControls);
 
     // inventory label
     SpriteFont inventoryLabel = new SpriteFont("Inventory", -250, 290, new Font("Times New Roman", Font.BOLD, 30),
         Color.ORANGE);
-    container.addComponent(inventoryLabel);
+    posContainer.addComponent(inventoryLabel);
 
     // inventory controls
     SpriteFont inventoryControls = new SpriteFont("1-2-3-4-`", 250, 290, new Font("Times New Roman", Font.BOLD, 30),
         Color.ORANGE);
-    container.addComponent(inventoryControls);
+    posContainer.addComponent(inventoryControls);
 
     // drop label
     SpriteFont dropLabel = new SpriteFont("Drop Item", -250, 360, new Font("Times New Roman", Font.BOLD, 30),
         Color.ORANGE);
-    container.addComponent(dropLabel);
+    posContainer.addComponent(dropLabel);
 
     // drop controls
     SpriteFont dropControls = new SpriteFont("R", 250, 360, new Font("Times New Roman", Font.BOLD, 30), Color.ORANGE);
-    container.addComponent(dropControls);
+    posContainer.addComponent(dropControls);
 
     // this will be the exit button
     this.exitButton = new AnimatedSpriteButton(10, 10, 5,
@@ -101,7 +100,6 @@ public class ControlsScreen extends Screen {
 
         });
 
-    posContainer.addComponent(container);
   }
 
   @Override
@@ -113,8 +111,8 @@ public class ControlsScreen extends Screen {
   @Override
   public void draw(GraphicsHandler graphicsHandler) {
     background.draw(graphicsHandler);
-    exitButton.draw(graphicsHandler);
     posContainer.draw(graphicsHandler);
+    exitButton.draw(graphicsHandler);
   }
 
 }
