1d012d313c6f49f12b04c03dc1b83787020b46d4
Tim McGinley <timmcginley0325@gmail.com>
2023-11-02 19:29:01 -0400

    -Unique Scripts for all wild west NPC's

--- src/Scripts/WildWestMap/CowboyThreeScript.java
+++ src/Scripts/WildWestMap/CowboyThreeScript.java
@@ -1,5 +1,49 @@
 package Scripts.WildWestMap;
 
-public class CowboyThreeScript {
-    
+import Level.NPC;
+import Level.Script;
+import Level.ScriptState;
+
+// script for talking to walrus npc
+public class CowboyThreeScript extends Script<NPC> {
+
+    @Override
+    protected void setup() {
+        System.out.println("Walrus script started");
+        lockPlayer();
+        showTextbox();
+
+        // changes what walrus says when talking to him the first time (flag is not set) vs talking to him afterwards (flag is set)
+        if (!isFlagSet("hasTalkedToWalrus")) {
+            addTextToTextboxQueue( "Good day to you!");
+            addTextToTextboxQueue( "Hope you're enjoying your time in Westville...");
+            addTextToTextboxQueue( "If ya haven't already, check out the saloon!");
+            addTextToTextboxQueue( "Good luck fella...");
+        }
+        else {
+            addTextToTextboxQueue("Hello there...");
+            addTextToTextboxQueue("Not much else to say...");
+            addTextToTextboxQueue("Have a blessed day!");
+        }
+        entity.facePlayer(player);
+    }
+
+    @Override
+    protected void cleanup() {
+        unlockPlayer();
+        hideTextbox();
+
+        // set flag so that if walrus is talked to again after the first time, what he says changes
+        setFlag("hasTalkedToWalrus");
+    }
+
+    @Override
+    public ScriptState execute() {
+        start();
+        if (!isTextboxQueueEmpty()) {
+            return ScriptState.RUNNING;
+        }
+        end();
+        return ScriptState.COMPLETED;
+    }
 }
