6608d0277f2fe2c7be58c63d92eb4ce9a28511f1
Zoe McLaughlin <zmmclaughlin@quinnipiac.edu>
2023-11-02 19:37:57 -0400

    fixes wrong mad scientist script

--- src/Maps/ScienceLabMap.java
+++ src/Maps/ScienceLabMap.java
@@ -77,7 +77,7 @@ public class ScienceLabMap extends Map
     {
         ArrayList<Trigger> triggers = new ArrayList<>();
         //just comment out the line below if you want to test the map without the trigger
-       triggers.add(new Trigger(770, 1042, 50,10, new triggerForScientist(), "hasTalkedToScientist"));
+       triggers.add(new Trigger(770, 1042, 50,10, new triggerForScientistScriptV2(), "hasTalkedToScientist"));
         return triggers;
     }
 
