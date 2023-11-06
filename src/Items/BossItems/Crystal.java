6f79b0115482560ff6a733c9bb1f382add6ee03b
Alex Power <alexpower9@gmail.com>
2023-11-06 15:15:38 -0500

    Item table now works

--- src/Items/BossItems/Crystal.java
+++ src/Items/BossItems/Crystal.java
@@ -1,8 +1,27 @@
 package Items.BossItems;
 
+import Engine.GraphicsHandler;
 import GameObject.Item;
+import Scripts.NewMap.SwordScript;
 
 public class Crystal extends Item
 {
-    
+    String pathToImage = "crystal.png";
+
+    public Crystal(int id, Utils.Point point)
+    {
+        super(id,point,"crystal.png");
+        this.setIsUncollidable(true);
+        this.setInteractScript(new SwordScript());
+    }
+
+    @Override
+    public void draw(GraphicsHandler graphicsHandler) {
+        super.draw(graphicsHandler);
+    }
+
+    public String getPathToImage()
+    {
+        return pathToImage;
+    }
 }
--- src/Items/BossItems/Metal.java
+++ src/Items/BossItems/Metal.java
@@ -1,8 +1,28 @@
 package Items.BossItems;
 
+import Engine.GraphicsHandler;
 import GameObject.Item;
+import Scripts.NewMap.SwordScript;
 
 public class Metal extends Item
 {
-    
+    String pathToImage = "rare_metal.png";
+
+    public Metal(int id, Utils.Point point)
+    {
+        super(id,point,"rare_metal.png");
+        this.setIsUncollidable(true);
+        //this.setInteractScript(new SwordScript());
+    }
+
+    @Override
+    public void draw(GraphicsHandler graphicsHandler) {
+        super.draw(graphicsHandler);
+    }
+
+    public String getPathToImage()
+    {
+        return pathToImage;
+    }
 }
+
