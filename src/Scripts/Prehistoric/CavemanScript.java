289950ecf4e9ee92426251849ecc94e226ef9de3
Zoe McLaughlin <zmmclaughlin@quinnipiac.edu>
2023-11-09 00:24:47 -0500

    he's here

---
+++ src/Scripts/Prehistoric/CavemanScript.java
@@ -0,0 +1,52 @@
+package Scripts.Prehistoric;
+
+import Level.GlobalFlagManager;
+import Level.NPC;
+import Level.Script;
+import Level.ScriptState;
+
+
+// script for talking to walrus npc
+public class CavemanScript extends Script<NPC> {
+
+
+    @Override
+    protected void setup() {
+        lockPlayer();
+        // changes what walrus says when talking to him the first time (flag is not set) vs talking to him afterwards (flag is set)
+        if (GlobalFlagManager.FLAG_MANAGER.isFlagSet("hasTalkedToCaveman")) {
+            showTextbox();
+            addTextToTextboxQueue("Well kid... you beat me fair and square.");
+            addTextToTextboxQueue("You can have my trusty crystal, but I\nnever want to see you again.");
+        }
+        else if (!GlobalFlagManager.FLAG_MANAGER.isFlagSet("hasTalkedToCaveman")) {
+            showTextbox();
+            addTextToTextboxQueue( "Hey, what're you doing here, kid?");
+            addTextToTextboxQueue( "...You don't look like you belong round these parts.");
+            addTextToTextboxQueue( "Either way... this is my patch of grass here, fella.");
+            addTextToTextboxQueue( "Get the hell out...");
+            addTextToTextboxQueue( "Get the hell out...\nbefore I make you!");
+        }
+        entity.facePlayer(player);
+    }
+
+    @Override
+    protected void cleanup() {
+        unlockPlayer();
+        hideTextbox();
+
+        // set flag so that if walrus is talked to again after the first time, what he says changes
+        GlobalFlagManager.FLAG_MANAGER.setFlag("hasTalkedToCaveman");
+    }
+
+    @Override
+    public ScriptState execute() {
+        start();
+        if (!isTextboxQueueEmpty()) {
+            return ScriptState.RUNNING;
+        }
+        end();
+        return ScriptState.COMPLETED;
+    }
+}
+
