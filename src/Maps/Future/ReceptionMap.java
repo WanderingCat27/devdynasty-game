7efba0f0775770eb13c7fb0de48d809df460df2e
Chris <cjkilduff@quinnipiac.edu>
2023-11-10 16:15:26 -0500

    Updated future lab work

--- src/Maps/Future/ReceptionMap.java
+++ src/Maps/Future/ReceptionMap.java
@@ -16,16 +16,16 @@ public class ReceptionMap extends Map{
   public ReceptionMap() {
     super("reception_map.txt", new FutureIndoorTileset());
     addMusic("Resources/Audio/saloon.wav");
-    this.playerStartPosition = getMapTile(8, 16).getLocation();
+    this.playerStartPosition = getMapTile(9, 16).getLocation();
     setCenterCamera();
   }
 
   @Override
   public ArrayList<Item> loadItems() {
     ArrayList<Item> list = new ArrayList<>();
-    BuildingEntrance elevator = new BuildingEntrance(0, getMapTile(8, 2).getLocation(), 160, 32, "floor1");
+    BuildingEntrance elevator = new BuildingEntrance(0, getMapTile(13, 2).getLocation(), 160, 32, "floor1");
     list.add(elevator);
-    BuildingEntrance receptionExit = new BuildingEntrance(0, getMapTile(7, 16).getLocation(), 160, 32, "future");
+    BuildingEntrance receptionExit = new BuildingEntrance(0, getMapTile(8, 16).getLocation(), 160, 32, "future");
     list.add(receptionExit);
     return list;
   }
