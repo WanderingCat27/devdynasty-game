48888e096121f9777ae3ce51ee0b892c50e8c81e
Alex Power <alexpower9@gmail.com>
2023-11-27 15:07:34 -0500

    Added animation for combat

--- src/ui/Container/UIContainer.java
+++ src/ui/Container/UIContainer.java
@@ -184,4 +184,26 @@ public class UIContainer {
   public ArrayList<UIContainer> children() {
     return components;
   }
+
+  public void setX(int x) {
+    this.x = x;
+  }
+
+  public void setY(int y) {
+    this.y = y;
+
+  }
+
+  public int getX() {
+    return x;
+  }
+
+  public int getY() {
+    return y;
+  }
+
+  public void setLocation(int x, int y) {
+    setX(x);
+    setY(y);
+  }
 }
